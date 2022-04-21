f0e44d97c21cda7fdcdff9d91c6cc1de9d992df7
Miha <malta_i04@mail.ru>
2022-04-21 21:30:21 +0700

    3 commit

--- main.py
+++ main.py
@@ -215,4 +215,5 @@ def upload():
 if __name__ == '__main__':
     db_session.global_init("аttractions.db")
     app.register_blueprint(attracts_api.blueprint)
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
